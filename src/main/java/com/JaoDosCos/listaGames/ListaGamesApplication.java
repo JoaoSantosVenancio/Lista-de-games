c81c41b6bcbee1922fdb8352bd3d0dc971bb96ca
JoãoSantos <jv29645@gmail.com>
2023-07-24 00:34:05 -0300

    initial commit

---
+++ src/main/java/com/JaoDosCos/listaGames/ListaGamesApplication.java
@@ -0,0 +1,13 @@
+package com.JaoDosCos.listaGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaGamesApplication.class, args);
+	}
+
+}
