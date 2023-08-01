cb23124a82b4e3c33ea00f73c1ba1491c4823d71
JoãoSantos <jv29645@gmail.com>
2023-08-01 20:03:20 -0300

    domain model

--- src/main/java/com/JaoDosCos/listaGames/entities/Game.java
+++ src/main/java/com/JaoDosCos/listaGames/entities/Game.java
@@ -7,13 +7,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name="tb_game")
 public class Game {
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String title;
+	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
